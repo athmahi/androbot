2e83fd70e4c8f2b75d55953483cf421cf69f86c2
athmahi <atharvamahindrakar@gmail.com>
2019-06-13 14:01:18 -0700

    some comments added

--- Software/freertos_hello_world.c
+++ Software/freertos_hello_world.c
@@ -1,9 +1,13 @@
-//@author
+/*
+* @file androbot.c
+* @author Atharva Mahindrakar
+* @author Prasanna Kulkarni
+* @Surya Ravikumar
+* @author Kanna Lakshamanan
+* @copyright Portland State University 2019*/
 
 
 
-
-/* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
@@ -49,29 +53,18 @@
 #define NX4IO_BASEADDR		XPAR_NEXYS4IO_0_S00_AXI_BASEADDR
 #define NX4IO_HIGHADDR		XPAR_NEXYS4IO_0_S00_AXI_HIGHADDR
 
-// Definitions for peripheral PMODOLEDRGB
-#define RGBDSPLY_DEVICE_ID		XPAR_PMODOLEDRGB_0_DEVICE_ID
-#define RGBDSPLY_GPIO_BASEADDR	XPAR_PMODOLEDRGB_0_AXI_LITE_GPIO_BASEADDR
-#define RGBDSPLY_GPIO_HIGHADDR	XPAR_PMODOLEDRGB_0_AXI_LITE_GPIO_HIGHADD
-#define RGBDSPLY_SPI_BASEADDR	XPAR_PMODOLEDRGB_0_AXI_LITE_SPI_BASEADDR
-#define RGBDSPLY_SPI_HIGHADDR	XPAR_PMODOLEDRGB_0_AXI_LITE_SPI_HIGHADDR
-
 
 //UARTLite
-
 #define UARTLITE_DEVICE_ID	XPAR_AXI_UARTLITE_1_DEVICE_ID
-
 #define TEST_BUFFER_SIZE    	784
-
 #define TEST_BUFFER_SIZE_1    	10
-
 #define TEST_BUFFER_SIZE_2    	1
 
 // Definitions for peripheral PMODHB3
-#define PMODHB3_0_BASEADDR		0x44A10000
-#define PMODHB3_1_BASEADDR		0x44A20000
+#define PMODHB3_0_BASEADDR		0x44A10000              // PMODHB3 for right motor
+#define PMODHB3_1_BASEADDR		0x44A20000              // PMODHB3 for left motor
 
-#define INTC_DEVICE_ID				XPAR_INTC_0_DEVICE_ID
+#define INTC_DEVICE_ID				XPAR_INTC_0_DEVICE_ID   // Interruot device ID
 
 // Definitions for peripheral GPIO 0
 #define GPIO_0_DEVICE_ID			XPAR_AXI_GPIO_0_DEVICE_ID
@@ -80,104 +73,152 @@
 
 
 //Create Instances
-//Function Declarations
 
-//Declare a Sempahore
-
-XTmrCtr		AXITimerInst;
+XTmrCtr		AXITimerInst;               // AXI timer instance
 XIntc 		IntrptCtlrInst;				// Interrupt Controller instance
-XUartLite   UartLite;
+XUartLite   UartLite;                   // UARTlite instance
 XGpio		GPIOInst0;					// GPIO instance for GPIO 0
 
+/*
+*    The structure is defined to store the Bot's parameters like
+*    current ON/OFF switch data, mode of operation and the headings.
+*    Defining a structure of these parameters helps access these values
+*    throughout the code. 
+*/
+
+
 typedef struct{
-	u32 uart_in[2];
-	u8	on_off_status;
-	u8	mode_operation;
-	u8	direction_from_app;
-	u8	headding;
-	u8	prev_heading;
+	u32 uart_in[2];             // uart's RX data
+	u8	on_off_status;          // ON/OFF switch data
+	u8	mode_operation;         // mode of operation data
+	u8	direction_from_app;     // direction commands received from app when in accelerometer mode
+	u8	headding;               // current heading
+	u8	prev_heading;           // previous heading
 }UART;
 
 UART uart;
 
+/*
+*   UART parameters data and its significance on the operation of the software
+*   on_off_status -> if 1 then bot is stopped and turned off
+*                    if 2 then botis turned on
+*   mode_operation ->  if 1 then in accelerometer mode
+*                      if 2 then in obstacle detection and mapping mode
+*
+*   direction_from_app -> if 1 then stop
+*                         if 2 then go forward
+*                         if 3 then go reverse
+*                         if 4 then turn right
+*                         if 5 then turn left
+*
+*   heading ->  if 1 then current heading is EAST
+*               if 2 then current heading is NORTH
+*               IF 3 then current heading is WEST
+*               IF 4 then current heading is SOUTH
+*
+*   Now for example if we want to run out bot in acceleromer mode and it should move forward, 
+*   then UART data sent from NodeMCU would be like - > 212
+*   it is decoded as hundredth place's "2" gives us ON/OFF switch status, for here "2" means bot is turned on
+*   the tenth place's "1" denotes mode of operation, here it means bot is in acceleromer mode
+*   the unit place's "2" denotes direction of movement, here it is "2" it means move forward. The same algorithm is 
+*   followed for rest of the directions too.
+*
+*   Now for Obstacle detection mode, the hundredth and tenth place's data is interpreted the same way as explained above.
+*   for example, we would receive data -> 22x from node MCU. 
+*   But here any directional data sent from NOdeMCU which is at unit's place is discarded. And after each iteration of the 
+*   program when in obstacle detection mode the current heading is sent to NodeMCU via UART. It is further interpreted 
+*   in NOdeMCU's code for updating the next X, Y cordinates of the Bot's movement.
+*/                      
 
-void reset_bot();
-void Decode_uart();
-void accelerometer_mode();
-void obstacle_detection_mode();
-int calculate_heading(int heading,int turn);
+
+
+void androbot();                                // Projects iterative function
+void reset_bot();                               // resets structure
+void Decode_uart();                             // decodes the UART data as explained in above comment section
+void accelerometer_mode();                      // accelerometer's mode function
+void obstacle_detection_mode();                 // obstacle detecction's mode function
+int calculate_heading(int heading,int turn);    // function to calculate next heading when in obstacle detection mode
 u8 obstacle[10];
 u8 heading[5];
 u8 count = 0;
+
+
+
 int main(void)
 {
 
 	uint32_t sts;
 
-	u8 data[10];
-	u8 datarcv[2];
-	u16 gpio_in = 0;
-
-
-	sts = do_init();
+	sts = do_init();                // initialises the  HW
 	if (XST_SUCCESS != sts)
 	{
 		exit(1);
 	}
 
-	xil_printf("Hello from FreeRTOS Example\r\n");
 
-
-	xil_printf(" uart test\n");
-	data[0] = 1;
-
-	//datarcv[0] = 1;
-
-	reset_bot();
+	reset_bot();                    // resets the Bot's parameters to default values
 	heading[0] = uart.headding;
 
-	while(1)
-	{
-		int rcv = XUartLite_Recv(&UartLite,datarcv,1);
-		uart.uart_in[0] = datarcv[0];
-		xil_printf(" uart rcvd - %d\n ", uart.uart_in[0]);
+    androbot();                     // starts the bot 
 
-
-
-		Decode_uart();
-
-		if(uart.on_off_status == 1)
-			reset_bot();
-
-		else
-		{
-			if(uart.mode_operation == 1)
-			{
-				xil_printf("in accelerometer mode\n\n");
-				accelerometer_mode();
-			}
-
-			else if(uart.mode_operation == 2)
-			{
-				xil_printf("in obstacle detection mode\n\n");
-				obstacle_detection_mode();
-			}
-		}
-
-		//XUartLite_ResetFifos2(&UartLite);
-
-
-	}
+	
 
 	return 0;
 }
 
 
+/*
+*   This is a iterative function. In this function, UART's data is read by polling it at every iteration.
+*   Then decode function is called to decode the data and after this, the proper function having correct 
+*   mode of operation is called.
+*/
+
+void androbot()
+{
+    u8 data[10];
+	u8 datarcv[2];
+	u16 gpio_in = 0;
+
+    while(1)
+	{
+		int rcv = XUartLite_Recv(&UartLite,datarcv,1);          // receives uart data into "datarcv buffer"
+                                                                // the function return 1 or 0 depending upon sucessful transaction
+		uart.uart_in[0] = datarcv[0];                           // set the first byte of data to our bots uart data
+		xil_printf(" uart rcvd - %d\n ", uart.uart_in[0]);      
+
+		Decode_uart();                                          // function call to decode received uart data
+
+		if(uart.on_off_status == 1)                             // if hundredth place is one, that is bot is off
+			reset_bot();                                        // reset bot's parameters and stop.
+
+		else
+		{
+			if(uart.mode_operation == 1)                        // if mode of operation is 1
+			{                                                   // then mode operation is accelerometer mode 
+				xil_printf("in accelerometer mode\n\n");
+				accelerometer_mode();                           // call to acceleromer function    
+			}
+
+			else if(uart.mode_operation == 2)                   // if mode of operation is 2
+			{
+				xil_printf("in obstacle detection mode\n\n");   // then mode operation is obstacle detection mode
+				obstacle_detection_mode();                      // call to obstacle detection function
+			}
+		}
+
+	}
+}
+
+
+/*
+*   In this function mode of  operation and heaing are given default values
+*/
+
 void reset_bot()
 {
-	uart.mode_operation = 0;
-	uart.headding = 2;
-	uart.prev_heading = 2;
+	uart.mode_operation = 0;                // default value 0 is given, because this number is not assigned to any operation
+	uart.headding = 2;                      // default heading is reset to NORTH, hence 2 is assigned
+	uart.prev_heading = 2;                  // prev. heading is also set to NORTH
 
 	xil_printf("BOT RESET \n\n");
 }
@@ -445,17 +486,17 @@ int	 do_init(void)
 	}
 	//XGpio_SetDataDirection(&GPIOInst0, GPIO_0_INPUT_0_CHANNEL, 0xFF);
 
-	/*status = AXI_Timer1_initialize();
+	status = AXI_Timer1_initialize();
 	if (status != XST_SUCCESS)
 	{
 		return XST_FAILURE;
-	}*/
+	}
 
-	//xil_printf("\n\n****** AXI timer 1 initialised\n\n****");
+	xil_printf("\n\n****** AXI timer 1 initialised\n\n****");
 
 }
 
-/*
+
 int AXI_Timer1_initialize(void){
 
 	uint32_t status;    // status from Xilinx Lib calls
@@ -487,8 +528,7 @@ int AXI_Timer1_initialize(void){
 
 	XTmrCtr_Enable(AXI_TIMER1_BASEADDR, TmrCtrNumber1);
 	return XST_SUCCESS;
-}*/
-
+}
 
 
 
