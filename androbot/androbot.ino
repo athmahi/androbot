1796bcff5c1913ea90a07ac64ed76512000a5281
athmahi <atharvamahindrakar@gmail.com>
2019-06-13 19:38:17 -0700

    comments added

--- androbot/androbot.ino
+++ androbot/androbot.ino
@@ -21,16 +21,17 @@
 #include <FirebaseArduino.h>
 
 // Set these to run example.
-#define FIREBASE_HOST "androbot-1f671.firebaseio.com"
-#define FIREBASE_AUTH "hUsGMLUDD7cdrMuCYVBca4ENHzzu0bS8gwxybMqn"
-#define WIFI_SSID "PMK"
-#define WIFI_PASSWORD "prasanna"
+#define FIREBASE_HOST "androbot-1f671.firebaseio.com"             // project host link
+#define FIREBASE_AUTH "hUsGMLUDD7cdrMuCYVBca4ENHzzu0bS8gwxybMqn"  // project authentication link
+#define WIFI_SSID "rsurya07"                                      // wifi SSID
+#define WIFI_PASSWORD "1904Nansu"                                 // wifi password
 
 void setup() {
-  Serial.begin(115200);
+  
+  Serial.begin(115200);                                          // set UART baudrate
 
   // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                         // conntect to wifi
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -38,9 +39,12 @@ void setup() {
   }
   Serial.println();
   Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());                               // print the IP
   
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                 // authenticate to firebase
+
+  Firebase.setInt("X", 500);                                    // set the starting cordinates to 500, 500
+  Firebase.setInt("Y", 500);
    
 }
 
@@ -54,70 +58,83 @@ int n = 0, x = 0, y = 0;
 void loop() {
 
   
-  n = Firebase.getInt("botOn");
+  n = Firebase.getInt("botOn");                             // read the ON/OFF switch data fromfirebase
   byte_sent = n * 100;
 
-  n = Firebase.getInt("autoMode");
+  n = Firebase.getInt("autoMode");                          // read the mode of operation
   byte_sent = byte_sent + (n * 10);
 
-  n = Firebase.getInt("directionfromApp");
+  n = Firebase.getInt("directionfromApp");                  // read the direction from app
 
   byte_sent = byte_sent + n;
 
   //Serial.print(" firebase data is - ");
-  Serial.write(byte_sent);
+  
+  Serial.write(byte_sent);                                  // send the byte of data to nexys4 board
 
-  if(Serial.available())
+  if(Serial.available())                                    // 1st latching of serial port
   {
 
-    while(Serial.available())
+    while(Serial.available())                               // 2nd latching of serial port
     {
-      byte_recv = Serial.read();
+      byte_recv = Serial.read();                            // read the data sent from Nexys4 board
       
     }
 
-    Serial.end();
-    Serial.begin(115200);
+    Serial.end();                                           // end the serial connection to clear Rx buffer
+    Serial.begin(115200);                                   // initialise the serial port and assign baudrate
   }
 
   heading = (int)byte_recv;
 
-  if(heading == 1)
+  if(heading == 1)                                          // if the heading is east
   {
-    x = Firebase.getInt("X");
-    x = x + 1;
-    Firebase.setInt("X", x);
-    Firebase.setInt("pointSet", 1);
+    x = Firebase.getInt("X");                               // read the previous X cordinate
+    if( x<1000 && x>0)                                      // check if bot is crossing the map boundry
+      x = x + 20;                                           // increment the pixelcount by 20
+    Firebase.setInt("X", x);                                // update the new X cordinate
+    Firebase.setInt("pointSet", 1);                         // set update bit to 1 so that android app
+                                                            // knows new data is being written
   }
 
-  if(heading == 2)
-  {
-    y = Firebase.getInt("Y");
-    y = y + 1;
-    Firebase.setInt("Y", y);
-    Firebase.setInt("pointSet", 1);
+  if(heading == 2)                                          // if heading is north
+  {   
+    y = Firebase.getInt("Y");                               // read the previous Y cordinate
+    if( y<1000 && y>0)                                      // check if bot is crossing the map boundry
+      y = y - 20;                                           // decrement the pixelcount by 20
+    Firebase.setInt("Y", y);                                // update the new Y cordinate
+    Firebase.setInt("pointSet", 1);                         // set update bit to 1 so that android app
+                                                            // knows new data is being written
   }
 
-  if(heading == 3)
+  if(heading == 3)                                          // if heading is West
   {
-    x = Firebase.getInt("X");
-    x = x - 1;
-    Firebase.setInt("X", x);
-    Firebase.setInt("pointSet", 1);
+    x = Firebase.getInt("X");                               // read the previous X cordinate
+    if( x<1000 && x>0)                                      // check if bot is crossing the map boundry
+      x = x - 20;                                           // decrement the pixelcount by 20
+    Firebase.setInt("X", x);                                // update the new X cordinate
+    Firebase.setInt("pointSet", 1);                         // set update bit to 1 so that android app
+                                                            // knows new data is being written
   }
 
-  if(heading == 4)
+  if(heading == 4)                                          // if heading is south
   {
-    y = Firebase.getInt("Y");
-    y = y - 1;
-    Firebase.setInt("Y", y);
-    Firebase.setInt("pointSet", 1);
+    y = Firebase.getInt("Y");                               // read the previous Y cordinate
+    if( y<1000 && y>0)                                      // check if bot is crossing the map boundry
+      y = y + 20;                                           // increment the pixelcount by 20
+    Firebase.setInt("Y", y);                                // update the new Y cordinate
+    Firebase.setInt("pointSet", 1);                         // set update bit to 1 so that android app
+                                                            // knows new data is being written
   }
-  
-  
-  Firebase.setInt("pointSet", 0);
-  Serial.flush();
-  delay(100);
+
+  if(heading == 5)                                          // if data received is stop bit
+  {
+    continue;                                               // continue statement is used because 
+                                                            // we don't want toupdate cordinates
+  }
+
+  Serial.flush();                                         // flush TX buffer of uart
+  delay(100);                                             // delay of 100 ms
   
  
 }
