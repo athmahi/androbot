ba05ab5d92c2d4c1017c10679cb2e1d8a37926f9
prasannamk <46940954+prasannamk@users.noreply.github.com>
2019-06-13 21:51:34 -0700

    Comments

--- hardware/pid_embsys_wrapper.v
+++ hardware/pid_embsys_wrapper.v
@@ -1,21 +1,33 @@
-//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
-//--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
-//Date        : Tue Jun  4 13:33:41 2019
-//Host        : LAPTOP-JK1HHNI2 running 64-bit major release  (build 9200)
-//Command     : generate_target pid_embsys_wrapper.bd
-//Design      : pid_embsys_wrapper
-//Purpose     : IP block netlist
-//--------------------------------------------------------------------------------
+// pid_embsys_wrapper.v - Top level module for Embedded System
+// 
+// Created By:	Prasanna Kulkarni 
+// Date:	18-April-2019
+// Version:	1.0
+//
+// Description:
+// ------------
+/*
+    The objective for this implementation is to create a SoC system 
+    on the Nexys4 DDR FPGA, that can drive a robot wirelessly from a app on an Android phone. 
+    Specifically, the idea is to use the ESP8266 NodeMCU to read the Firebase database 
+    and drive a bot according to the elements in the database and the mode of operation.
+//////////////////////////////////////////////////////////////////////
+*/
 `timescale 1 ps / 1 ps
 
 module pid_embsys_wrapper
-   (an_0,
-    btnC_0,
+   (
+    an_0,
+    // Nexys4 Input Push Buttons
+    ////////////////////////////
+    btnC_0,                     
     btnD_0,
     btnL_0,
     btnR_0,
     btnU_0,
+    ////////////////////////////
+    // Nexys4 DDR2 Memory 
+    ////////////////////////////
     ddr2_sdram_addr,
     ddr2_sdram_ba,
     ddr2_sdram_cas_n,
@@ -30,18 +42,26 @@ module pid_embsys_wrapper
     ddr2_sdram_odt,
     ddr2_sdram_ras_n,
     ddr2_sdram_we_n,
+    ////////////////////////////
     dp_0,
     led_0,
     reset,
     seg_0,
     sw_0,
     sys_clock,
+    // Debug UART Signals 
+    ////////////////////////////
     usb_uart_rxd,
     usb_uart_txd,
+    ////////////////////////////
+    // PMOD Conector Headers
+    ////////////////////////////
     JA,
     JB,
     JC,
-    JD);
+    JD
+    ////////////////////////////
+    );
   wire [15:0]GPIO_0_tri_i;
   output [7:0]an_0;
   input btnC_0;
@@ -107,22 +127,40 @@ module pid_embsys_wrapper
   wire [6:0]seg_0;
   wire [15:0]sw_0;
   wire sys_clock;
-  wire usb_uart_1_rxd;
-  wire usb_uart_1_txd;
   wire usb_uart_rxd;
   wire usb_uart_txd;
 
-
+  ////////// UART ////////////////
+  // UART Connections are made here. The JC pin is considered
+  // as an inout allowing the same header to be used for receive and
+  // transmit pins
+  ////////////////////////////
   assign  usb_uart_1_rxd=JC[0]; 
   assign JC[1]=usb_uart_1_txd;
-  assign JB[0]=dir_0;                       
-  assign JB[1]=pwm_0;                                 
-  assign sa_input_0=JB[2];
-  assign JA[0]=dir_1;                       
-  assign JA[1]=pwm_1;                                 
-  assign sa_input_1=JA[2];
-  assign GPIO_0_tri_i = {8'b0, JD};
+  ////////////////////////////
   
+  //////////PMOD HB3_0///////////////
+  // These are the signals for the PMOD HB3 drivers
+  // The sa input is not used but is still assigned 
+  // to prevent floasting errors,
+  ////////////////////////////
+  assign JB[0]=dir_0;                       
+  assign JB[1]=pwm_0;                                
+  assign sa_input_0=JB[2];
+  //////////////////////////// 
+  //////////////////////////// 
+  assign JA[0]=dir_1;                       
+  assign JA[1]=pwm_1;                                
+  assign sa_input_1=JA[2];
+  //////////////////////////// 
+  //////////////////////////// 
+  
+  ////////// GPIO //////////////
+  //This is the GPIO connection that we have used
+  // interface the IR LEDs.  
+  //////////////////////////// 
+  assign GPIO_0_tri_i = {8'b0, JD};
+  //////////////////////////// 
   
   pid_embsys pid_embsys_i
        (.GPIO_0_tri_i(GPIO_0_tri_i),
